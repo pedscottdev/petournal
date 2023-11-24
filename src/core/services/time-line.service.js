41b8ccb3a40212d76876b44b47e873a970aaa3e6
VuKiet <81238426+vubakiet@users.noreply.github.com>
2023-11-24 20:51:51 +0700

    handle feed and setting
    handle feed and setting

--- src/core/services/time-line.service.js
+++ src/core/services/time-line.service.js
@@ -1,8 +1,8 @@
 import axiosClient from "./base.js";
 
 const TimeLineService = {
-    getTimeLine: () => {
-        return axiosClient.get("/timeLine/getTimeLine");
+    getTimeLine: (body) => {
+        return axiosClient.post("/timeLine/getTimeLine", body);
     },
 };
 
